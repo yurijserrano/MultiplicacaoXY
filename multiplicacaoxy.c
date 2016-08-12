5610d52e3ae2a54b75659fd8520e484e27b636e6
Yuri Jose Dos Santos Serrano <yurijserrano@gmail.com>
2016-08-12 16:43:58 -0300

    Programa concluído

---
+++ multiplicacaoxy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	int x,y;
+	int multiplicacao;
+
+	printf("Digite o valor do X:");
+	scanf("%d",&x);
+
+	printf("Digite o valor do Y:");
+	scanf("%d",&y);
+
+
+	multiplicacao = x*y;
+
+	printf("O resultado da multiplicacao eh: %d\n",multiplicacao);
+
+	
+}
